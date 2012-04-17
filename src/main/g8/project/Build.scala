de82d302e42dbf2da6862fe859e0b1760d1e3301
Yoshiori SHOJI <yoshiori@gmail.com>
2012-04-18 01:55:28 +0900

    Specs2 と log周り追加

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -11,6 +11,14 @@ object ProjectBuild extends Build {
       organization := "$organization$",
       version := "$version$",
       scalaVersion := "$scala_version$"
+      libraryDependencies ++= Seq(
+        // test
+        "org.specs2" %% "specs2" % "1.9" % "test",
+            
+        //log
+        "org.clapper" %% "grizzled-slf4j" % "0.6.8",
+        "ch.qos.logback" % "logback-classic" % "1.0.1"
+      )
       // add other settings here
     )
   )
